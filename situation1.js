366597c3ff660fe6024e0efab306ceeb003c8664
ahw(linux) <mytory@gmail.com>
2018-11-06 18:34:04 +0900

    상황 1-1 해결

--- situation1.js
+++ situation1.js
@@ -1,2 +1,15 @@
 // 윈도우는 `Windows 10`, iOS는 `OS X`, 안드로이드는 `Android 8.0` 형식으로 os 값이 찍힌다.
 console.log(browserDetect());
+
+var app = new Vue({
+    el: '#app',
+    data: {
+        name: '',
+        account_name: ''
+    },
+    watch: {
+        name: function (value) {
+            this.account_name = value;
+        }
+    }
+});
