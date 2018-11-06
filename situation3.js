ae71586c945d63acdd432b30f4eddd7dd99f046c
ahw(linux) <mytory@gmail.com>
2018-11-06 19:14:54 +0900

    상황3 1,2,4 해결

--- situation3.js
+++ situation3.js
@@ -2,3 +2,27 @@ document.querySelector('#form').addEventListener('submit', function (e) {
     console.log('e.preventDefault()로 submit의 기본 동작을 막습니다.');
     e.preventDefault();
 });
+
+var app = new Vue({
+    el: '#app',
+    data: {
+        name: '',
+        amount: '',
+        list: [],
+        id: 1
+    },
+    methods: {
+        add: function (e) {
+            e.preventDefault();
+            var item = {
+                id: this.id,
+                name: this.name,
+                amount: this.amount
+            };
+            this.list.push(item);
+            this.name = '';
+            this.amount = '';
+            this.id++;
+        }
+    }
+});
